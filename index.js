2e2b43e7c02da856cbcd29637460e281d70fb3e1
munishk686 <munishk686@gmail.com>
2023-02-13 18:52:01 +1300

    second commit

--- index.js
+++ index.js
@@ -135,6 +135,11 @@ List.findOne({name: customListName}, function(err, foundList){
     res.render("about");
   });
 
-app.listen(3000, function() {
-  console.log("Server started on post 3000");
+  let port = process.env.PORT;
+  if (port == null || port == "") {
+    port = 3000;
+  }
+
+app.listen(port, function() {
+  console.log("Server has started on port 3000");
 });
